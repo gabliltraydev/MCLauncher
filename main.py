2f6f3c78bd796f38f69ec11d3a5e06630130ce6d
gabliltraydev <99366409+gabliltraydev@users.noreply.github.com>
2023-03-19 13:00:05 +0100

    cleaned up
    change var name and added while loops to avoid code repeatition

--- main.py
+++ main.py
@@ -1,64 +1,57 @@
-import tempfile
-import os
 import subprocess
 import ctypes
-import sys
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 import tkinter
 import uuid
 from wget import *
 from zipfile import *
 
-tmpfolder = tempfile.gettempdir()
+TEMP_FOLDER = tempfile.gettempdir()
+INSTALLATION_FOLDER = os.path.join(TEMP_FOLDER, "mcfolder.txt")
+DOWNLOAD_URL = "http://gamezipfile.duckdns.org/game.zip"
 
 
-def baseFolder():
-    if os.path.exists(tmpfolder + "/mcfolder.txt"):
-        with open(tmpfolder + "/mcfolder.txt", 'r') as f:
-            print(f.read())
-    else:
-        tkinter.messagebox.showinfo(title="Sélectionner un dossier",
-                                    message="Merci de choisir le dossier d'installation des fichiers de votre launcher.")
-        filename = filedialog.askdirectory()
-        print("doss: " + filename)
-
-        flpth = filename + "e.txt"
+def create_base_folder():
+    while not os.path.exists(INSTALLATION_FOLDER):
+        messagebox.showinfo(title="Sélectionner un dossier",
+                            message="Merci de choisir le dossier d'installation des fichiers de votre launcher.")
+        file_name = filedialog.askdirectory()
+        check_admin_file = file_name + "e.txt"
+        print(file_name)
         try:
-            filehandle = open(flpth, 'w')
+            filehandle = open(check_admin_file, 'w')
         except IOError:
             tkinter.messagebox.showerror(title="Erreur",
-                                         message="Dossier protégé par des autorisations administrateurs, merci de relancer le programme et de changer de dossier.")
+                                         message="Dossier protégé par des autorisations administrateurs, merci de "
+                                                 "relancer le programme et de changer de dossier.")
 
             sys.exit("protected")
 
-        with open(tmpfolder + "/mcfolder.txt", 'w') as f:
-            f.write(filename)
-
-
-def checkFolderExists():
-    while os.path.getsize(tmpfolder + "/mcfolder.txt") == 0:
-        tkinter.messagebox.showerror(title="Erreur",
-                                     message="Vous n'avez pas indiqué de chemin, merci d'indiquer un chemin d'installation")
+        with open(TEMP_FOLDER + "/mcfolder.txt", 'w') as f:
+            f.write(file_name)
+            
+    while os.path.getsize(TEMP_FOLDER + "/mcfolder.txt") == 0:
+        messagebox.showerror(title="Erreur",
+                             message="Vous n'avez pas indiqué de chemin, merci d'indiquer un chemin d'installation")
 
         filename = filedialog.askdirectory()
         print("doss: " + filename)
 
-        flpth = filename + "e.txt"
+        file_path = filename + "_check_admin.txt"
         try:
-            filehandle = open(flpth, 'w')
+            filehandle = open(file_path, 'w')
         except IOError:
-
-            ctypes.windll.user32.MessageBoxW(0,
-                                             "Dossier protégé par des autorisations administrateurs, merci de relancer le programme et de changer de dossier.",
-                                             "Erreur", 0x10)
+            messagebox.showerror(title="Erreur",
+                                 message="Dossier protégé par des autorisations administrateurs, merci de relancer le "
+                                         "programme et de changer de dossier.")
             sys.exit("protected")
 
-        with open(tmpfolder + "/mcfolder.txt", 'w') as f:
+        with open(TEMP_FOLDER + "/mcfolder.txt", 'w') as f:
             f.write(filename)
 
 
-def downloadExtractFiles():
-    with open(tmpfolder + "/mcfolder.txt", 'r') as f:
+def download_extract_files():
+    with open(TEMP_FOLDER + "/mcfolder.txt", 'r') as f:
         folder = f.read()
 
     if os.path.exists(folder + "/jeu.bat") & os.path.exists(folder + "/runtime") \
@@ -71,49 +64,48 @@ def downloadExtractFiles():
                                          "Lorsque vous cliquerez sur Ok, le téléchargement va commencer",
                                          "Téléchargement", 0)
 
-        filepath = folder + "/game.zip"
-        url = "http://gamezipfile.duckdns.org/game.zip"
-        download(url, folder)
+        file_path = folder + "/game.zip"
 
-        zip = ZipFile(filepath)
-        zip.extractall(folder)
+        download(DOWNLOAD_URL, folder)
+
+        zip_file_mc1_12_2 = ZipFile(file_path)
+        zip_file_mc1_12_2.extractall(folder)
 
 
-def launchVerOne():
-    downloadExtractFiles()
-    with open(tmpfolder + "/mcfolder.txt", 'r') as f:
+def launch_ver_one():
+    download_extract_files()
+    with open(TEMP_FOLDER + "/mcfolder.txt", 'r') as f:
         folder = f.read()
-    playerUUID = uuid.uuid1()
-    uuidstr = str(playerUUID)
-    print(uuidstr)
+    player_uuid = uuid.uuid1()
+    uuid_string = str(player_uuid)
+    print(uuid_string)
 
-    psd = pseudo.get()
+    player_pseudo = pseudo_field.get()
 
     with open(folder + "/nick.txt", 'w') as f:
-        f.write(psd)
+        f.write(player_pseudo)
     with open(folder + "/uuid.txt", 'w') as f:
-        f.write(uuidstr)
+        f.write(uuid_string)
 
     subprocess.call(folder + "/jeu.bat")
 
 
-baseFolder()
-checkFolderExists()
+create_base_folder()
 
 root = tkinter.Tk()
 root.geometry("900x600")
 root.title("Launcher")
 
-bgimg = tkinter.PhotoImage(file="bg.png")
-btnBg = tkinter.PhotoImage(file="playbytton.png")
-pseudo = tkinter.PhotoImage(file="playbytton.png")
+background_image = tkinter.PhotoImage(file="bg.png")
+button_background = tkinter.PhotoImage(file="playbytton.png")
 
-limg = tkinter.Label(root, i=bgimg)
-pseudo = tkinter.Entry(root, width=33)
-start = tkinter.Button(root, height='80px', width='109px', command=lambda: launchVerOne(), borderwidth=0, image = btnBg)
+window_background = tkinter.Label(root, i=background_image)
+pseudo_field = tkinter.Entry(root, width=33)
+start_button = tkinter.Button(root, height='80px', width='109px', command=lambda: launch_ver_one(), borderwidth=0,
+                              image=button_background)
 
-pseudo.place(x=245, y=291, in_=root)
-start.place(x=555, y=250, in_=root)
-limg.pack()
+pseudo_field.place(x=245, y=291, in_=root)
+start_button.place(x=555, y=250, in_=root)
+window_background.pack()
 
 root.mainloop()
